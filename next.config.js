c9dd985c9488d8f015bf03e429faf0b9d0ff0813
recosank <recosank009@gmail.com>
2023-03-05 12:56:20 +0530

    add search page

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  rules: {
+    "react/display-name": "off",
+  },
   images: {
     domains: [
       "i.scdn.co",
